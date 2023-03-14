8f926aca99af77a2aa4c5c2480e8de9d013bd379
NSTCG <nithinsteven32@gmail.com>
2023-03-14 17:56:58 +0530

    Migrate physics example to 1.0.0
    index.js : add index.js in js folder
    force-on-click.js : update the component to class
    package.json : add package.json with glmatrix dependancy
    package-lock.json : add package-lock.json

--- physics/force-on-click.js
+++ physics/force-on-click.js
@@ -1,18 +1,29 @@
-WL.registerComponent('apply-force-on-click', {
-    strength: {type: WL.Type.Float, default: 100.0},
-}, {
-    start: function() {
+import {Component, Type} from '@wonderlandengine/api';
+import * as glMatrix from 'gl-matrix';
+
+export class ApplyForceOnClick extends Component {
+    static TypeName = 'apply-force-on-click';
+    static Properties = {
+        strength: {type: Type.Float, default: 100.0},
+    };
+
+    start() {
         const target = this.object.getComponent('cursor').globalTarget;
         target.addClickFunction(this.onClick.bind(this));
 
         this.force = new Float32Array(3);
-    },
+    }
 
-    onClick: function(object, cursor) {
+    onClick(object, cursor) {
         const body = object.getComponent('physx');
-        if(body.static) return;
+        if (body.static) return;
 
         glMatrix.vec3.scale(this.force, cursor.direction, this.strength);
-        body.addForce(this.force, WL.ForceMode.Force, false, cursor.rayHit.locations[0]);
-    },
-});
+        body.addForce(
+            this.force,
+            this.engine.ForceMode.Force,
+            false,
+            cursor.rayHit.locations[0]
+        );
+    }
+}
