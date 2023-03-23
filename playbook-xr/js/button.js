1c5fa013c8f6abaef6f5b008339c89b8b0966f12
NSTCG <nithinsteven32@gmail.com>
2023-03-23 11:11:34 +0530

    Migrate playbook-xr example to 1.0.0

--- playbook-xr/js/button.js
+++ playbook-xr/js/button.js
@@ -1,56 +1,70 @@
-WL.registerComponent('button', {
-    buttonMeshObject: {type: WL.Type.Object},
-    hoverMaterial: {type: WL.Type.Material},
-}, {
-    start: function() {
+import {Component, Type} from '@wonderlandengine/api';
+
+export class Button extends Component {
+    static TypeName = 'button';
+    static Properties = {
+        buttonMeshObject: {type: Type.Object},
+        hoverMaterial: {type: Type.Material},
+    };
+
+    start() {
         this.mesh = this.buttonMeshObject.getComponent('mesh');
         this.defaultMaterial = this.mesh.material;
 
-        this.target = this.object.getComponent('cursor-target') || this.object.addComponent('cursor-target');
+        this.target =
+            this.object.getComponent('cursor-target') ||
+            this.object.addComponent('cursor-target');
         this.target.addHoverFunction(this.onHover.bind(this));
         this.target.addUnHoverFunction(this.onUnHover.bind(this));
         this.target.addDownFunction(this.onDown.bind(this));
         this.target.addUpFunction(this.onUp.bind(this));
 
-        this.soundClick = this.object.addComponent('howler-audio-source', {src: 'sfx/click.wav', spatial: true});
-        this.soundUnClick = this.object.addComponent('howler-audio-source', {src: 'sfx/unclick.wav', spatial: true});
-    },
+        this.soundClick = this.object.addComponent('howler-audio-source', {
+            src: 'sfx/click.wav',
+            spatial: true,
+        });
+        this.soundUnClick = this.object.addComponent('howler-audio-source', {
+            src: 'sfx/unclick.wav',
+            spatial: true,
+        });
+    }
 
-    onHover: function(_, cursor) {
+    onHover(_, cursor) {
         this.mesh.material = this.hoverMaterial;
-        if(cursor.type == 'finger-cursor') {
+        if (cursor.type == 'finger-cursor') {
             this.onDown(_, cursor);
         }
 
         this.hapticFeedback(cursor.object, 0.5, 50);
-    },
+    }
 
-    onDown: function(_, cursor) {
+    onDown(_, cursor) {
         this.soundClick.play();
         this.buttonMeshObject.translate([0.0, 0.0, 0.02]);
         this.hapticFeedback(cursor.object, 1.0, 20);
-    },
+    }
 
-    onUp: function(_, cursor) {
+    onUp(_, cursor) {
         this.soundUnClick.play();
         this.buttonMeshObject.translate([0.0, 0.0, -0.02]);
         this.hapticFeedback(cursor.object, 0.7, 20);
-    },
+    }
 
-    onUnHover: function(_, cursor) {
+    onUnHover(_, cursor) {
         this.mesh.material = this.defaultMaterial;
-        if(cursor.type == 'finger-cursor') {
+        if (cursor.type == 'finger-cursor') {
             this.onUp(_, cursor);
         }
 
         this.hapticFeedback(cursor.object, 0.3, 50);
-    },
+    }
 
-    hapticFeedback: function(object, strength, duration) {
+    hapticFeedback(object, strength, duration) {
         const input = object.getComponent('input');
-        if(input && input.xrInputSource) {
+        if (input && input.xrInputSource) {
             const gamepad = input.xrInputSource.gamepad;
-            if(gamepad && gamepad.hapticActuators) gamepad.hapticActuators[0].pulse(strength, duration);
+            if (gamepad && gamepad.hapticActuators)
+                gamepad.hapticActuators[0].pulse(strength, duration);
         }
-    },
-});
+    }
+}
--- playbook-xr/js/playbook-toggle.js
+++ playbook-xr/js/playbook-toggle.js
@@ -1,32 +1,41 @@
-WL.registerComponent('playbook-toggle', {
-    param: {type: WL.Type.Float, default: 1.0},
-}, {
+import {Component, Type} from '@wonderlandengine/api';
+
+export class PlaybookToggle extends Component {
+    static TypeName = 'playbook-toggle';
+    static Properties = {
+        param: {type: Type.Float, default: 1.0},
+    };
+
     init() {
         this.toggled = false;
-    },
+    }
+
     start() {
-        this.target = this.object.getComponent('cursor-target') || this.object.addComponent('cursor-target');
-        this.collision = this.object.getComponent('collision') || this.object.addComponent('collision');
-        this.collision.extents = [ 0.07, 0.04, 0.02 ];
+        this.target =
+            this.object.getComponent('cursor-target') ||
+            this.object.addComponent('cursor-target');
+        this.collision =
+            this.object.getComponent('collision') || this.object.addComponent('collision');
+        this.collision.extents = [0.07, 0.04, 0.02];
         this.collision.collider = WL.Collider.AxisAlignedBox;
-        this.collision.group = 0xF;
+        this.collision.group = 0xf;
         this.collision.active = true;
         this.target.addClickFunction(this.onClick.bind(this));
         console.log(this.collision);
 
         const children = this.object.children;
-        for(const c of children) {
-            if(c.name == 'knob') this.knob = c;
+        for (const c of children) {
+            if (c.name == 'knob') this.knob = c;
         }
-    },
+    }
 
     onClick() {
         this.toggled = !this.toggled;
-        const moveAmount = 0.030;
-        if(this.toggled) {
+        const moveAmount = 0.03;
+        if (this.toggled) {
             this.knob.translate([-moveAmount, 0, 0]);
         } else {
             this.knob.translate([moveAmount, 0, 0]);
         }
     }
-});
+}
