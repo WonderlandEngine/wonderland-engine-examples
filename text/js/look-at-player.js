092cfe58db664bb0756c3bb6dd4c34a2ae9579b9
NSTCG <nithinsteven32@gmail.com>
2023-03-23 19:16:00 +0530

    Migrate text exapmple to 1.0.0

--- text/js/look-at-player.js
+++ text/js/look-at-player.js
@@ -1,20 +1,25 @@
-WL.registerComponent('look-at-player', {
-    player: {type: WL.Type.Object, default: null},
-}, {
-    init: function() {
+import {Component, Type} from '@wonderlandengine/api';
+
+export class LookAtPlayer extends Component {
+    static TypeName = 'look-at-player';
+    static Properties = {
+        player: {type: Type.Object, default: null},
+    };
+
+    init() {
         this.translation = [0, 0, 0];
         this.forward = [0, 0, 0];
-    },
-    
-    update: function(dt) {
+    }
+
+    update(dt) {
         this.player.getTranslationWorld(this.translation);
         this.object.lookAt(this.translation, [0, 1, 0]);
 
         // Set position, based on FOV?
         this.player.getForward(this.forward);
-        for(i = 0; i < 3; ++i) {
-            this.translation[i] += this.forward[i]*3.0;
+        for (i = 0; i < 3; ++i) {
+            this.translation[i] += this.forward[i] * 3.0;
         }
         //this.object.setTranslationWorld(this.translation);
-    },
-});
+    }
+}
--- text/js/text-scroller.js
+++ text/js/text-scroller.js
@@ -1,18 +1,23 @@
-WL.registerComponent('text-scroller', {
-    speed: {type: WL.Type.Float, default: 0.5},
-}, {
-    init: function() {
+import {Component, Type} from '@wonderlandengine/api';
+
+export class TextScroller extends Component {
+    static TypeName = 'text-scroller';
+    static Properties = {
+        speed: {type: Type.Float, default: 0.5},
+    };
+
+    init() {
         this.time = 0.0;
         this.textLength = 0;
-    },
-    
-    update: function(dt) {
+    }
+
+    update(dt) {
         this.time += dt;
-        if(this.time > this.speed) {
+        if (this.time > this.speed) {
             const str = 'Welcome to Wonderland!';
             this.object.getComponent('text').text = str.substring(0, this.textLength);
             this.textLength = (this.textLength + 1) % (str.length + 1);
             this.time = 0.0;
         }
-    },
-});
+    }
+}
