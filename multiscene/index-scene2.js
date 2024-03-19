dae517db0262d7374d30eca7356bab44f7635ce3
NSTCG <nithinsteven32@gmail.com>
2024-03-19 14:40:51 +0530

    Add multiscene example.

---
+++ multiscene/index-scene2.js
@@ -0,0 +1,25 @@
+/* wle:auto-imports:start */
+import {Cursor} from '@wonderlandengine/components';
+import {HandTracking} from '@wonderlandengine/components';
+import {HowlerAudioListener} from '@wonderlandengine/components';
+import {MouseLookComponent} from '@wonderlandengine/components';
+import {PlayerHeight} from '@wonderlandengine/components';
+import {TeleportComponent} from '@wonderlandengine/components';
+import {VrModeActiveSwitch} from '@wonderlandengine/components';
+import {WasdControlsComponent} from '@wonderlandengine/components';
+import {SwitchScene} from './js/switch-scene.js';
+/* wle:auto-imports:end */
+
+export default function registerSceneComponents(engine) {
+	/* wle:auto-register:start */
+engine.registerComponent(Cursor);
+engine.registerComponent(HandTracking);
+engine.registerComponent(HowlerAudioListener);
+engine.registerComponent(MouseLookComponent);
+engine.registerComponent(PlayerHeight);
+engine.registerComponent(TeleportComponent);
+engine.registerComponent(VrModeActiveSwitch);
+engine.registerComponent(WasdControlsComponent);
+engine.registerComponent(SwitchScene);
+/* wle:auto-register:end */
+}
---
+++ multiscene/index.js
@@ -0,0 +1,127 @@
+/**
+ * /!\ This file is auto-generated.
+ *
+ * This is the entry point of your standalone application.
+ *
+ * There are multiple tags used by the editor to inject code automatically:
+ *     - `wle:auto-imports:start` and `wle:auto-imports:end`: The list of import statements
+ *     - `wle:auto-register:start` and `wle:auto-register:end`: The list of component to register
+ *     - `wle:auto-constants:start` and `wle:auto-constants:end`: The project's constants,
+ *        such as the project's name, whether it should use the physx runtime, etc...
+ *     - `wle:auto-benchmark:start` and `wle:auto-benchmark:end`: Append the benchmarking code
+ */
+
+/* wle:auto-imports:start */
+import {Cursor} from '@wonderlandengine/components';
+import {FingerCursor} from '@wonderlandengine/components';
+import {HandTracking} from '@wonderlandengine/components';
+import {HowlerAudioListener} from '@wonderlandengine/components';
+import {MouseLookComponent} from '@wonderlandengine/components';
+import {PlayerHeight} from '@wonderlandengine/components';
+import {TeleportComponent} from '@wonderlandengine/components';
+import {VrModeActiveSwitch} from '@wonderlandengine/components';
+import {WasdControlsComponent} from '@wonderlandengine/components';
+import {SwitchScene} from './js/switch-scene.js';
+/* wle:auto-imports:end */
+
+import {loadRuntime} from "@wonderlandengine/api";
+import * as API from "@wonderlandengine/api"; // Deprecated: Backward compatibility.
+
+/* wle:auto-constants:start */
+const Constants = {
+    ProjectName: 'MultisceneExample.wlp',
+    RuntimeBaseName: 'WonderlandRuntime',
+    WebXRRequiredFeatures: ['local',],
+    WebXROptionalFeatures: ['local','local-floor','hand-tracking','hit-test',],
+};
+const RuntimeOptions = {
+    physx: false,
+    loader: false,
+    xrFramebufferScaleFactor: 1,
+    xrOfferSession: {
+        mode: 'auto',
+        features: Constants.WebXRRequiredFeatures,
+        optionalFeatures: Constants.WebXROptionalFeatures,
+    },
+    canvas: 'canvas',
+};
+/* wle:auto-constants:end */
+
+RuntimeOptions.threads = false;
+
+const engine = await loadRuntime(Constants.RuntimeBaseName, RuntimeOptions);
+Object.assign(engine, API); // Deprecated: Backward compatibility.
+window.WL = engine; // Deprecated: Backward compatibility.
+
+engine.onSceneActivated.once(() => {
+	const el = document.getElementById("version");
+	if (el) setTimeout(() => el.remove(), 2000);
+});
+
+/* WebXR setup. */
+
+function requestSession(mode) {
+	engine
+		.requestXRSession(
+			mode,
+			Constants.WebXRRequiredFeatures,
+			Constants.WebXROptionalFeatures,
+		)
+		.catch((e) => console.error(e));
+}
+
+function setupButtonsXR() {
+	/* Setup AR / VR buttons */
+	const arButton = document.getElementById("ar-button");
+	if (arButton) {
+		arButton.dataset.supported = engine.arSupported;
+		arButton.addEventListener("click", () =>
+			requestSession("immersive-ar"),
+		);
+	}
+	const vrButton = document.getElementById("vr-button");
+	if (vrButton) {
+		vrButton.dataset.supported = engine.vrSupported;
+		vrButton.addEventListener("click", () =>
+			requestSession("immersive-vr"),
+		);
+	}
+}
+
+if (document.readyState === "loading") {
+	window.addEventListener("load", setupButtonsXR);
+} else {
+	setupButtonsXR();
+}
+
+/* wle:auto-register:start */
+engine.registerComponent(Cursor);
+engine.registerComponent(FingerCursor);
+engine.registerComponent(HandTracking);
+engine.registerComponent(HowlerAudioListener);
+engine.registerComponent(MouseLookComponent);
+engine.registerComponent(PlayerHeight);
+engine.registerComponent(TeleportComponent);
+engine.registerComponent(VrModeActiveSwitch);
+engine.registerComponent(WasdControlsComponent);
+engine.registerComponent(SwitchScene);
+/* wle:auto-register:end */
+
+let sceneGroup;
+try {
+	sceneGroup = await engine.loadSceneGroup(`${Constants.ProjectName}.bin`);
+	const scene = sceneGroup.getScene(0);
+	scene.activate();
+} catch (e) {
+	console.error(e);
+	window.alert(`Failed to load ${Constants.ProjectName}.bin:`, e);
+}
+
+/* Load second scene */
+engine.loadSceneGroup("scene2.wlp.bin");
+
+/* wle:auto-benchmark:start */
+/* wle:auto-benchmark:end */
+
+import registerSceneComponents from "./index-scene2.js";
+registerSceneComponents(engine);
