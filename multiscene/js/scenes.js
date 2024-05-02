feea077f202966a49b8f05659d653cf35f39541a
NSTCG <nithinsteven32@gmail.com>
2024-05-02 21:19:01 +0530

    Update multiscene example

---
+++ multiscene/js/scenes.js
@@ -0,0 +1,7 @@
+/** Contains the globally loaded scenes. */
+export const Scenes = {
+    /** Main scene, loaded first. */
+    main: null,
+    /** Magical scene, reached via the portal */
+    second: null
+};
--- multiscene/js/switch.js
+++ multiscene/js/switch.js
@@ -1,17 +1,12 @@
 import {Component} from '@wonderlandengine/api';
+import {Scenes} from './scenes.js';
 
-/**
- * switch-scene
- */
+/** Component to switch between the main and second scene. */
 export class SwitchScene extends Component {
-    static TypeName = 'switch-scene';
-    switch() {
-        const scene0 = this.engine.getSceneGroup(0).getScene(0);
-        const scene1 = this.engine.getSceneGroup(1).getScene(0);
-        if (this.scene === scene1) {
-            scene0.activate();
-        } else {
-            scene1.activate();
-        }
-    }
+	static TypeName = 'switch-scene';
+
+	switch() {
+		const nextScene = this.scene === Scenes.main ? Scenes.second : Scenes.main;
+		this.engine.switchTo(nextScene);
+	}
 }
