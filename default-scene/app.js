d1dda919b3e0ccff365d2745c93a9caecfcca2b5
Hugo Amiard <hugo.amiard@wonderlandengine.com>
2024-11-05 13:57:38 +0900

    default-scene: Add DefaultScene example

---
+++ default-scene/app.js
@@ -0,0 +1,86 @@
+/**
+ * /!\ This file is auto-generated.
+ *
+ * This is the entry point of your standalone application.
+ *
+ * There are multiple tags used by the editor to inject code automatically:
+ *     - `wle:auto-constants:start` and `wle:auto-constants:end`: The project's constants,
+ *        such as the project's name, whether it should use the physx runtime, etc...
+ *     - `wle:auto-benchmark:start` and `wle:auto-benchmark:end`: Append the benchmarking code
+ */
+
+import {loadRuntime} from '@wonderlandengine/api';
+
+/* wle:auto-constants:start */
+const Constants = {
+    ProjectName: 'DefaultScene',
+    RuntimeBaseName: 'WonderlandRuntime',
+    WebXRRequiredFeatures: ['local',],
+    WebXROptionalFeatures: ['local','hand-tracking','hit-test',],
+};
+const RuntimeOptions = {
+    physx: false,
+    loader: false,
+    xrFramebufferScaleFactor: 1,
+    xrOfferSession: {
+        mode: 'auto',
+        features: Constants.WebXRRequiredFeatures,
+        optionalFeatures: Constants.WebXROptionalFeatures,
+    },
+    canvas: 'canvas',
+};
+/* wle:auto-constants:end */
+
+RuntimeOptions.webgpu = true;
+
+const engine = await loadRuntime(Constants.RuntimeBaseName, RuntimeOptions);
+
+engine.onLoadingScreenEnd.once(() => {
+    const el = document.getElementById('version');
+    if (el) setTimeout(() => el.remove(), 2000);
+});
+
+const nextFrame = () => {
+    engine.nextFrame();
+    window.requestAnimationFrame(nextFrame);
+};
+window.requestAnimationFrame(nextFrame);
+
+/* WebXR setup. */
+
+function requestSession(mode) {
+    engine
+        .requestXRSession(mode, Constants.WebXRRequiredFeatures, Constants.WebXROptionalFeatures)
+        .catch((e) => console.error(e));
+}
+
+function setupButtonsXR() {
+    /* Setup AR / VR buttons */
+    const arButton = document.getElementById('ar-button');
+    if (arButton) {
+        arButton.setAttribute('data-supported', engine.arSupported.toString());
+        arButton.addEventListener('click', () => requestSession('immersive-ar'));
+    }
+    const vrButton = document.getElementById('vr-button');
+    if (vrButton) {
+        vrButton.setAttribute('data-supported', engine.vrSupported.toString());
+        vrButton.addEventListener('click', () => requestSession('immersive-vr'));
+    }
+}
+
+if (document.readyState === 'loading') {
+    window.addEventListener('load', setupButtonsXR);
+} else {
+    setupButtonsXR();
+}
+
+/* Load main scene */
+try {
+    await engine.loadMainScene(`${Constants.ProjectName}.bin`);
+} catch (e) {
+    console.error(e);
+    window.alert(`Failed to load ${Constants.ProjectName}.bin:`, e);
+}
+
+/* wle:auto-benchmark:start */
+/* wle:auto-benchmark:end */
---
+++ default-scene/test/app.js
@@ -0,0 +1,26 @@
+import {runScreenshotTest} from '../../test-utils.js';
+
+/* wle:auto-constants:start */
+const Constants = {
+    ProjectName: 'DefaultScene',
+    RuntimeBaseName: 'WonderlandRuntime',
+    WebXRRequiredFeatures: ['local',],
+    WebXROptionalFeatures: ['local','hand-tracking','hit-test',],
+};
+const RuntimeOptions = {
+    physx: false,
+    loader: false,
+    xrFramebufferScaleFactor: 1,
+    xrOfferSession: {
+        mode: 'auto',
+        features: Constants.WebXRRequiredFeatures,
+        optionalFeatures: Constants.WebXROptionalFeatures,
+    },
+    canvas: 'canvas',
+};
+/* wle:auto-constants:end */
+
+RuntimeOptions.threads = false; /* Disabled for testing on any browser */
+RuntimeOptions.simd = false;
+
+await runScreenshotTest(Constants.ProjectName, Constants.RuntimeBaseName, RuntimeOptions);
