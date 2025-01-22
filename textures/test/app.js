43f287ff0386172feed65efdad8f2ea47e11e878
Hugo Amiard <hugo.amiard@wonderlandengine.com>
2025-01-22 17:11:38 +0100

    textures: Add Textures (DefaultScene with basic Textured Phong)

---
+++ textures/test/app.js
@@ -0,0 +1,26 @@
+import {runScreenshotTestsMultiConfig} from '../../test-utils.js';
+
+/* wle:auto-constants:start */
+const Constants = {
+    ProjectName: 'Textures',
+    RuntimeBaseName: 'WonderlandRuntime',
+    WebXRRequiredFeatures: ['local',],
+    WebXROptionalFeatures: ['local','hand-tracking','hit-test',],
+};
+const RuntimeOptions = {
+    physx: false,
+    loader: false,
+    xrFramebufferScaleFactor: 1,
+    xrOfferSession: {
+        mode: 'auto',
+        features: Constants.WebXRRequiredFeatures,
+        optionalFeatures: Constants.WebXROptionalFeatures,
+    },
+    canvas: 'canvas',
+};
+/* wle:auto-constants:end */
+
+RuntimeOptions.threads = false; /* Disabled for testing on any browser */
+RuntimeOptions.simd = false;
+
+await runScreenshotTestsMultiConfig(Constants.ProjectName, Constants.RuntimeBaseName, RuntimeOptions, ['webgl2', 'webxr', 'webgpu']);
