36a224cb7be50bb458e8df9affb9bf294eec148a
Timmy Kokke <mental_c@hotmail.com>
2025-09-26 13:24:26 +0200

    update gitignore + remove incorrectly cached files

--- mixed-reality/cache/mixed-reality/js/_editor_index.js
+++
@@ -1,8 +0,0 @@
-import * as __wonderlandengine_components from '@wonderlandengine/components';
-_registerEditor(__wonderlandengine_components);
-import * as _e__WLE_wonderland_engine_examples_mixed_reality_js_button_ts from 'e:/WLE/wonderland-engine-examples/mixed-reality/js/button.ts';
-_registerEditor(_e__WLE_wonderland_engine_examples_mixed_reality_js_button_ts);
-import * as _e__WLE_wonderland_engine_examples_mixed_reality_js_place_shape_anchor_js from 'e:/WLE/wonderland-engine-examples/mixed-reality/js/place-shape-anchor.js';
-_registerEditor(_e__WLE_wonderland_engine_examples_mixed_reality_js_place_shape_anchor_js);
-import * as _e__WLE_wonderland_engine_examples_mixed_reality_js_random_plane_colors_js from 'e:/WLE/wonderland-engine-examples/mixed-reality/js/random-plane-colors.js';
-_registerEditor(_e__WLE_wonderland_engine_examples_mixed_reality_js_random_plane_colors_js);
